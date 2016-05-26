295819fc2948b0a8f749401e357c49cfa34e73b2
Kuptz. Marian <m.kuptz@seeburger.de>
2016-05-26 17:51:52 +0200

    Geolocation 2

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -8,7 +8,9 @@
  function register_event_handlers()
  {
 
-     alert("hallo");
+    GPSCheckUp();
+
+
      /* graphic button  #btnFoto */
     $(document).on("click", "#btnFoto", function(evt)
     {
@@ -25,5 +27,33 @@
     });
 
     }
+
+    //Überprüfen ob GPS akiv ist
+    function GPSCheckUp(){
+
+      var onSuccess = function(position) {
+        alert('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');
+    };
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+
+    }
+
+
  document.addEventListener("app.Ready", register_event_handlers, false);
 })();
