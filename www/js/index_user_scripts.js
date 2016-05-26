3257d7e11bdfe51b92c7368daf8a096d8dde3360
Kuptz. Marian <m.kuptz@seeburger.de>
2016-05-26 17:59:57 +0200

    GPS Abfrage zu Beginn funktioniert!

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -32,14 +32,12 @@
     function GPSCheckUp(){
 
       var onSuccess = function(position) {
-        alert('Latitude: '          + position.coords.latitude          + '\n' +
-              'Longitude: '         + position.coords.longitude         + '\n' +
-              'Altitude: '          + position.coords.altitude          + '\n' +
-              'Accuracy: '          + position.coords.accuracy          + '\n' +
-              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-              'Heading: '           + position.coords.heading           + '\n' +
-              'Speed: '             + position.coords.speed             + '\n' +
-              'Timestamp: '         + position.timestamp                + '\n');
+        /*alert('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n'
+             );*/
+        $('#txtGPSHome').append('<p>GPS ist aktiv!</p>');
+
+
     };
 
     // onError Callback receives a PositionError object
@@ -53,6 +51,7 @@
 
 
     }
+
 
 
  document.addEventListener("app.Ready", register_event_handlers, false);
